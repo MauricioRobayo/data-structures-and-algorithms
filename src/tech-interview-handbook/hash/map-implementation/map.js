65708e46eada8d30fd96279afa9d1c7b4788e7d3
Mauricio Robayo <hi@mauriciorobayo.com>
2020-02-06 18:46:54 -0500

    Add hash table problem

---
+++ src/tech-interview-handbook/hash/map-implementation/map.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-underscore-dangle */
+/*
+  Implement data structure `Map` storing pairs of integers (key, value)
+  and define following member functions in O(1) runtime:
+    - void insert(key, value)
+    - void delete(key)
+    - int get(key)
+    - int getRandomKey()
+*/
+const DEFAULT_TABLE_SIZE = 20
+
+class Map {
+  constructor(size = DEFAULT_TABLE_SIZE) {
+    this._tableSize = size
+    this.table = Array.from({ length: this._tableSize }, () => [])
+  }
+
+  static hash(key, size = DEFAULT_TABLE_SIZE) {
+    let idx = 0
+    for (let i = 0; i < key.length; i += 1) {
+      idx += key.charCodeAt(i)
+    }
+    return idx % size
+  }
+
+  get tableSize() {
+    return this._tableSize
+  }
+
+  insert(key, value) {
+    const idx = Map.hash(key)
+    const bucket = this.table[idx]
+    if (bucket.length === 0) {
+      bucket.push([key, value])
+    }
+    if (!bucket.find(([k]) => k === key)) {
+      bucket.push([key, value])
+    }
+  }
+
+  delete(key) {
+    const idx = Map.hash(key)
+    const bucket = this.table[idx]
+    for (let i = 0; i < bucket.length; i += 1) {
+      if (bucket[i][0] === key) {
+        return bucket.splice(i, 1)[0][1]
+      }
+    }
+    return null
+  }
+
+  get(key) {
+    const idx = Map.hash(key)
+    const element = this.table[idx].find(([k]) => k === key)
+    return element === undefined ? element : element[1]
+  }
+}
+
+module.exports = Map
---
+++ src/tech-interview-handbook/hash/map-implementation/mapGetRandomKey.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-underscore-dangle */
+/*
+  Implement data structure `Map` storing pairs of integers (key, value)
+  and define following member functions in O(1) runtime:
+    - void insert(key, value)
+    - void delete(key)
+    - int get(key)
+    - int getRandomKey()
+*/
+class Map {
+  constructor() {
+    this.table = {}
+    this.keys = []
+    this.keysLength = 0
+  }
+
+  insert(key, value) {
+    if (this.table[key] !== undefined) {
+      return
+    }
+    this.table[key] = [value, this.keysLength]
+    this.keys.push(key)
+    this.keysLength += 1
+  }
+
+  delete(key) {
+    if (this.table[key] === undefined) {
+      return undefined
+    }
+    const [value, index] = this.table[key]
+    delete this.table[key]
+    ;[this.keys[index], this.keys[this.keysLength - 1]] = [
+      this.keys[this.keysLength - 1],
+      this.keys[index],
+    ]
+    this.keysLength -= 1
+    return value
+  }
+
+  get(key) {
+    const value = this.table[key]
+    return value === undefined ? undefined : value[0]
+  }
+
+  getRandomKey(randFn = Math.random) {
+    console.log(Math.floor(randFn() * this.keysLength))
+    console.log(this.keys, this.keysLength)
+    return this.keys[Math.floor(randFn() * this.keysLength)]
+  }
+}
+
+module.exports = Map
