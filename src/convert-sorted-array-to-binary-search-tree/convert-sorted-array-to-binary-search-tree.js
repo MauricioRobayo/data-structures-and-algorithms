f42528ec0ad7e76d1e8682bde992352b3e8ffe88
Mauricio Robayo <rfmajo@gmail.com>
2020-09-01 07:50:03 -0500

    Add convert-sorted-array-to-binary-search-tree

---
+++ src/convert-sorted-array-to-binary-search-tree/convert-sorted-array-to-binary-search-tree.js
@@ -0,0 +1,27 @@
+function TreeNode(val = 0, left = null, right = null) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+}
+
+const sortedArrayToBST = function (nums) {
+  if (nums.length === 0) {
+    return null;
+  }
+
+  const arrMiddle = Math.floor(nums.length / 2);
+  const middleEl = nums[arrMiddle];
+
+  const node = new TreeNode(middleEl);
+
+  console.log(arrMiddle);
+  console.log('left:', nums.slice(0, arrMiddle));
+  console.log('right:', nums.slice(arrMiddle + 1));
+
+  node.left = sortedArrayToBST(nums.slice(0, arrMiddle));
+  node.right = sortedArrayToBST(nums.slice(arrMiddle + 1));
+
+  return node;
+};
+
+console.log(sortedArrayToBST([-10, -3, 0, 5, 9]));
