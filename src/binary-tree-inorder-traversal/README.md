95d7ca20b7f1ee9413c11bfc05b0e0ef86cb4980
Mauricio Robayo <rfmajo@gmail.com>
2020-09-01 09:43:50 -0500

    Add binary-tree-inorder-traversal problem

---
+++ src/binary-tree-inorder-traversal/README.md
@@ -0,0 +1,22 @@
+## Binary Tree In-order Traversal
+
+Given a binary tree, return the in-order traversal of its nodes' values.
+
+Example:
+
+```
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+```
+
+Output: [1,3,2]
+
+**Follow up**: Recursive solution is trivial, could you do it iteratively?
+
+## Source
+
+https://leetcode.com/problems/binary-tree-inorder-traversal/
---
+++ src/binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line
+function TreeNode(val = 0, left = null, right) {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+}
+
+// eslint-disable-next-line
+function bst(array) {}
+
+function inOrderTraversal(root) {
+  const res = [];
+  const stack = [];
+  let curr = root;
+
+  while (stack.length > 0 || curr) {
+    while (curr) {
+      stack.push(curr);
+      curr = curr.left;
+    }
+    curr = stack.pop();
+    res.push(curr.val);
+    curr = curr.right;
+  }
+  return res;
+}
+
+const array = [1, null, 2, 3];
+const tree = bst(array);
+console.log(inOrderTraversal(tree));
