6d59c20c78144138e581d133557c1a2342fe2fef
Mauricio Robayo <rfmajo@gmail.com>
2020-09-06 06:46:55 -0500

    Less restrictive ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,12 @@
 module.exports = {
-  env: {
-    jest: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: ['airbnb-base', 'prettier'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    'no-console': 'off',
-    'func-names': 'off',
-  },
-}
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": ["eslint:recommended", "prettier"],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
