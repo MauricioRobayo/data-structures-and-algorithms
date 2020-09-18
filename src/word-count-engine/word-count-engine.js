600a713913eb3bbd97c4d36a02665975160a4fb9
Mauricio Robayo <rfmajo@gmail.com>
2020-09-18 11:03:10 -0500

    Fix word-count-engine solution

--- src/word-count-engine/word-count-engine.js
+++ src/word-count-engine/word-count-engine.js
@@ -1,44 +1,47 @@
 function wordCountEngine(document) {
-  const wordFrequency = new Map();
-  const order = [];
-  const result = [];
-
-  document
-    .trim()
-    .toLowerCase()
-    .replace(/[^a-z ]/g, '')
-    .split(' ')
-    .forEach((word) => {
-      if (wordFrequency.has(word)) {
-        wordFrequency.set(word, wordFrequency.get(word) + 1);
-      } else {
-        wordFrequency.set(word, 1);
-      }
-    });
-
-  wordFrequency.forEach((val, key) => {
-    if (order[val] === undefined) {
-      order[val] = [key];
-    } else {
-      order[val].push(key);
+  // Build array of normalized words
+  const normalizedWords = [];
+  document.split(' ').forEach((word) => {
+    if (word !== '') {
+      normalizedWords.push(word.toLowerCase().replace(/[^a-z]/g, ''));
     }
   });
 
-  for (let i = order.length - 1; i >= 0; i -= 1) {
-    const words = order[i];
-    if (order[i] === undefined) {
-      continue; // eslint-disable-line no-continue
+  // build a map of the frequencies O(n)
+  const wordFrequencies = new Map();
+  normalizedWords.forEach((word) => {
+    if (wordFrequencies.has(word)) {
+      wordFrequencies.set(word, wordFrequencies.get(word) + 1);
+    } else {
+      wordFrequencies.set(word, 1);
     }
-    words.forEach((word) => {
-      result.push([word, String(i)]);
-    });
+  });
+
+  // build and array with frequencies as indexes and words as an array
+  const sortedFrequencies = [];
+  wordFrequencies.forEach((frequency, word) => {
+    if (sortedFrequencies[frequency] === undefined) {
+      sortedFrequencies[frequency] = [word];
+    } else {
+      sortedFrequencies[frequency].push(word);
+    }
+  });
+
+  // unwind frequencies array
+  const result = [];
+  for (let i = sortedFrequencies.length - 1; i >= 0; i--) {
+    if (Array.isArray(sortedFrequencies[i])) {
+      sortedFrequencies[i].forEach((word) => {
+        result.push([word, String(i)]);
+      });
+    }
   }
 
   return result;
 }
 
-const document =
-  'Every book is a quotation; and every house is a quotation out of all forests, and mines, and stone quarries; and every man is a quotation from all his ancestors. ';
-const wordCount = wordCountEngine(document);
+const doc =
+  'Every book is a quotation; and every house is a        quotation out of all forests, and mines, and stone quarries; and every man is a quotation from all his ancestors. ';
+const wordCount = wordCountEngine(doc);
 
 console.log(wordCount);
