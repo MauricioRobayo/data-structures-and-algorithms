54a343a40bea13ad1045a7bef4203563eb2eccaf
Mauricio Robayo <rfmajo@gmail.com>
2020-09-18 09:35:46 -0500

    O(n) solution to array-of-array-products

--- src/array-of-array-products/array-of-array-products.js
+++ src/array-of-array-products/array-of-array-products.js
@@ -1,32 +1,22 @@
-function getProduct(arr) {
-  return arr.reduce((product, value) => product * value, 1);
-}
-
 function arrayOfArrayProducts(arr) {
-  const result = [];
-
   if (arr.length <= 1) {
-    return result;
+    return [];
   }
 
-  const left = [];
-  for (let i = 1; i < arr.length; i++) {
-    left.push(getProduct(arr.slice(i)));
-  }
-  left.push(1);
+  const result = [1];
 
-  const right = [];
-  for (let i = arr.length - 1; i >= 1; i--) {
-    right.unshift(getProduct(arr.slice(0, i)));
+  for (let i = 0; i < arr.length - 1; i++) {
+    result.push(result[i] * arr[i]);
   }
-  right.unshift(1);
 
-  for (let i = 0; i < arr.length; i++) {
-    result.push(left[i] * right[i]);
+  let product = 1;
+  for (let i = arr.length - 1; i > 0; i--) {
+    product *= arr[i];
+    result[i - 1] = result[i - 1] * product;
   }
 
   return result;
 }
 
-const arr = [2, 7, 3, 4];
-console.log(arrayOfArrayProducts(arr));
+console.log(arrayOfArrayProducts([2, 7, 3, 4]));
+// output: [84, 24, 56, 42]
