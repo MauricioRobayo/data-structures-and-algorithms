5d588abaca84d9ae925d2770c4094e6fed3c6164
Mauricio Robayo <rfmajo@gmail.com>
2020-09-29 07:05:39 -0500

    Add getting-a-different-number O(n) time and constant space solution

--- src/getting-a-different-number/getting-a-different-number.js
+++ src/getting-a-different-number/getting-a-different-number.js
@@ -1,12 +1,20 @@
 function getDifferentNumber(arr) {
-  const set = new Set(arr);
-  let i = 0;
-
-  while (set.has(i)) {
-    i++;
+  for (let i = 0; i < arr.length; i++) {
+    const index = arr[i] === -Infinity ? 0 : Math.abs(arr[i]);
+    if (index < arr.length) {
+      if (arr[index] === 0) {
+        arr[index] = -Infinity;
+      } else arr[index] *= -1;
+    }
   }
 
-  return i;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0) {
+      return i;
+    }
+  }
+
+  return arr.length;
 }
 
 const arr = [0, 1, 2, 3];
